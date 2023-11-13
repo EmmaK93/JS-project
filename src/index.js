a1573f887f5f2de35bdb4afe109bb40f42e49b98
EmmaK93 <ems.93@hotmail.com>
2023-11-13 17:17:47 +0100

    Deleted apps

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
+import Home from './Views/Home';
+import Contact from './Views/Contact';
+import NotFound from './Views/NotFound';
 
-import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
