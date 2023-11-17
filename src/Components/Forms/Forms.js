1812693475af7a11b29db6c339f3702f36e87d20
EmmaK93 <ems.93@hotmail.com>
2023-11-17 16:42:12 +0100

    Forms html done

--- src/Components/Forms/Forms.js
+++ src/Components/Forms/Forms.js
@@ -1,38 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Forms.css'
-import Button from '../Generics/Buttons/Button'
+
 
 const Forms = () => {
 
+    const [name, setName] = useState('')
+    const [nameError, setNameError]=useState(false)
+    const [email, setEmail] = useState('')
+    const [emailError, setEmailError] = useState(false)
+    const [message, setMessage] = useState('')
+    const [errorMessage, setErrorMessage]= useState(false)
 
+    const validLength=(value, minLength=2)=>{
+        
+        if (value.length <= minLength)
+            return true
+        return false
+    }
+
+    const validateEmail=(value)=>{
+    
+        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/.test(value))
+            
+            return true
+     
+         return false
+        
+        
+    }
+
+    const handleChange =(e)=>{
+        switch (e.target.type){
+            case "text":
+                setName(e.target.value)
+                setNameError(validLength(e.target.value))
+
+            break
+            case "email":
+                setEmail(e.target.value)
+                setEmailError(validateEmail(e.target.value))
+            break
+
+            case "message":
+                setMessage(e.target.value)
+                setErrorMessage(validLength(e.target.value))
+            break
+        }
+
+
+    }
+
+   
+    
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        
+        const user = {name, email, message}
+        const json= JSON.stringify(user)
+        console.log(json)
+
+        const result= await fetch('https://win23.azurewebsites.net/api/users', {
+            method: 'post',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: json
+        },[])
+
+        
+
+        switch(result.status){
+            case 201:
+                alert('Användare skapas')
+                clearForms()
+            break
+            case 400:
+                console.log (`Felmeddelandet är: ${await result.text()}`)
+            break
+            default:
+                console.log ('Annat fel')
+        }
+
+        const clearForms =()=>{
+            setName('')
+            setEmail('')
+            setMessage('')
+        }
+    }
 
   return (
     <>
     <section className="container form-input">
             <h2>Leave us a message for any information.</h2>
-        <form className="forms" onsubmit="register(event)" method="post" novalidate>
+        <form className="forms" onSubmit={handleSubmit} noValidate>
             <div className="mb-5">
-                <input id="firstName" className="info-form" type="text" placeholder="Förnamn..." required onkeyup="validate(this)"/>
-                <span id="warning.firstName"></span> 
+                <label className={`${nameError ? 'error':''}`}> {`${nameError ? 'För- och efternamn måste anges':''}`}</label>
+                <input id="name" className="info-form" type="text"  placeholder="För- och efternamn..." required value={name} onChange={(e)=>handleChange(e)}/>
             </div>
             <div className="mb-5">
-                <input id="lastName" className="info-form" type="text" placeholder="Efternamn..." required onkeyup="validate(this)"/>
-                <span id="warning.lastName"></span> 
+                <label className={`${emailError ? 'error':''}`}>{`${emailError ? 'E-postadress måste vara giltig':''}`}</label>
+                <input id="email" className="info-form" type="email" placeholder="Epost..." required value={email} onChange={(e)=>handleChange(e)}/>
             </div>
             <div className="mb-5">
-                <input id="email" className="info-form" type="email" placeholder="Email..." required onkeyup="validate(this)"/>
-                <span id="warning.email"></span>
+                <label className={`${errorMessage ? 'error':''}`}>{`${errorMessage ? 'Meddelande måste innehålla minst 2 tecken':''}`}</label>
+                <textarea id="message" className="info-form" type="text"  placeholder="Meddelande..." required value={message} onChange={(e)=>handleChange(e)}></textarea> 
             </div>
-            <div className="mb-5">
-                <input id="passWord" className="info-form" type="password" placeholder="Lösenord..." required onkeyup="validate(this)"/>
-                <span id="warning.passWord">Måste innehålla en liten bokstav, siffra (0-9), specialtecken samt bestå av minst 8 tecken</span> 
+           <div className='mb-5'>
+            <button id="submit-btn" type="submit"className="info-form" required>Skicka</button>
             </div>
-            <div className="mb-5">
-                <input id="confirm" className="info-form" type="password" placeholder="Bekräfta lösenord..." required data-comparewith="passWord" onkeyup="validate(this)"/>
-                <span id="warning.confirm"></span> 
-            </div>
-            <Button type="yellow" Title="Skicka" url="/" id="submit-btn"/>
-            
         </form>
     </section>
     </>
