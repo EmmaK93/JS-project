4abaf85c125a47c073244f52fcb09b05771c1ee3
EmmaK93 <ems.93@hotmail.com>
2023-11-19 22:41:51 +0100

    fixed problem

--- src/Components/Sponsor/SponsorSection.js
+++ src/Components/Sponsor/SponsorSection.js
@@ -8,18 +8,18 @@ import img_7 from '../../Assets/images/7.svg'
 import img_6 from '../../Assets/images/6.svg'
 import img_9 from '../../Assets/images/9.svg'
 import img_10 from '../../Assets/images/10.svg'
-import { Link } from 'react-router-dom'
+
 
 
 const SponsorSection = () => {
   return (
     <section className="sponsor">
         <div className="container">
-            <img src={img_8} alt="Sponsor logotype"/><Link src="*"></Link>
-            <img src={img_7} alt="Sponsor logotype"/><Link src="*"></Link>
-            <img src={img_6} alt="Sponsor logotype"/><Link src="*"></Link>
-            <img src={img_9} alt="Sponsor logotype"/><Link src="*"></Link>
-            <img src={img_10} alt="Sponsor logotype"/><Link src="*"></Link>
+            <img src={img_8} alt="Sponsor logotype"/><Link to="*"></Link>
+            <img src={img_7} alt="Sponsor logotype"/><Link to="*"></Link>
+            <img src={img_6} alt="Sponsor logotype"/><Link to="*"></Link>
+            <img src={img_9} alt="Sponsor logotype"/><Link to="*"></Link>
+            <img src={img_10} alt="Sponsor logotype"/><Link to="*"></Link>
         </div>
         </section>
   )
